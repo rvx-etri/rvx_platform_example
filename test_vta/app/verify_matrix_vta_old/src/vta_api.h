a2ad4690306c548570b48e64424b5ee1b6744d2a
Kyuseung Han <han@etir.re.kr>
2025-07-02 16:48:55 +0900

    o. remove vta

--- test_vta/app/verify_matrix_vta_old/src/vta_api.h
+++
@@ -1,7 +0,0 @@
-#ifndef __VTA_API_H__
-#define __VTA_API_H__
-
-ervp_task_wait_fx_t matrix_mult_vta_16x16(ervp_mop_mapping_t *mop_mapping, const ErvpMatrixInfo *a, const ErvpMatrixInfo *b, ErvpMatrixInfo *c, int options);
-ervp_task_wait_fx_t matrix_mult_vta(ervp_mop_mapping_t *mop_mapping, const ErvpMatrixInfo *a, const ErvpMatrixInfo *b, ErvpMatrixInfo *c, int options);
-
-#endif
--- test_vta/user/vta/src/Adder_8.v
+++
@@ -1,7 +0,0 @@
-module Adder_8(
-  input  [65:0] io_a,
-  input  [65:0] io_b,
-  output [66:0] io_y
-);
-  assign io_y = $signed(io_a) + $signed(io_b); // @[TensorGemm.scala 81:13]
-endmodule
