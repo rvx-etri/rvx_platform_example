5e99c8346717294dc66cb61d5ce03a20f9b42a1d
Kyuseung Han <han@etir.re.kr>
2025-10-23 10:57:37 +0900

    o. update

--- test_video/app/hello/src/main.c
+++
@@ -1,8 +0,0 @@
-#include "platform_info.h"
-#include "ervp_printf.h"
-#include "ervp_multicore_synch.h"
-
-int main() {
-	printf("Hello World!\n");
-	return 0;
-}
--- test_video/user/api/tiger_small_jpg_file.h
+++
@@ -1,7 +0,0 @@
-#ifndef __TIGER_SMALL_JPG_FILE_H__
-#define __TIGER_SMALL_JPG_FILE_H__
-
-#include "ervp_fakefile.h"
-
-
-#endif
